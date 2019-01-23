b5c831f9a3a9e8f06821619bb07e7234e964b18a
Marcos Paulo <marcospss@gmail.com>
2019-01-23 20:39:45 -0200

    Heroku Dpeloyment.

--- libs/core/environments/environment.prod.ts
+++ libs/core/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     dbName: 'ionicNgrxMovieDB',
     storeName: 'favorites',
     apiEndpoint: 'https://api.themoviedb.org/3',
-    apikey: '--- API KEY ---',
+    apikey: 'd272326e467344029e68e3c4ff0b4059',
     language: 'en-US'
 };
