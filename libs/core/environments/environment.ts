13e4cfd963a59eea93a5976624c27ea62836d613
Marcos Paulo <marcospss@gmail.com>
2019-01-25 12:06:28 -0200

    Correção de conflitos

--- libs/core/environments/environment.ts
+++ libs/core/environments/environment.ts
@@ -4,10 +4,6 @@ export const environment = {
     dbName: 'ionicNgrxMovieDB',
     storeName: 'favorites',
     apiEndpoint: 'https://api.themoviedb.org/3',
-<<<<<<< HEAD
     apikey: '---- API KEY ----',
-=======
-    apikey: 'd272326e467344029e68e3c4ff0b4059',
->>>>>>> ae47d79f70f6602e3af6d4727b55bba2ca48f8cb
     language: 'en-US'
 };
